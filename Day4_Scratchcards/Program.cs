46c62db64c6a11014bd2032b8dd23f6b6ac13336
tobbox <me@tobb.dev>
2023-12-12 20:12:14 +0100

    Day 4 - Scratchcard

---
+++ Day4_Scratchcards/Program.cs
@@ -0,0 +1,33 @@
+﻿var totalValue = 0;
+
+using (var sr = new StreamReader("./Input.txt"))
+{
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        var timesWon = 0;
+
+        var separatorIndex = line.IndexOf(":");
+        var remainingPart = line.Substring(separatorIndex + 1);
+
+        var segments = remainingPart.Split("|");
+        var winningNumbers = segments[0].Split(" ");
+        var guessedNumbers = segments[1].Split(" ");
+
+        foreach (var number in winningNumbers)
+        {
+            if (guessedNumbers.Contains(number))
+            {
+                timesWon++;
+            }
+        }
+
+        if (timesWon > 1)
+        {
+            totalValue += (int)Math.Pow(2, timesWon - 1);
+        }
+    }
+}
+
+Console.WriteLine(totalValue);
