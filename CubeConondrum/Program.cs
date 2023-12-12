b743cba33d97c938992b25f6716001be3341791b
tobbox <me@tobb.dev>
2023-12-12 15:40:04 +0100

    Day 2 - Cubes; Part 1, 2

---
+++ CubeConondrum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.RegularExpressions;
+
+var gameSum = 0;
+
+using (var sr = new StreamReader("./Input.txt"))
+{
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        var gameNum = 0;
+        var invalidLine = false;
+
+        int totalRed, totalGreen, totalBlue;
+        totalRed = totalGreen = totalBlue = 0;
+
+        var inputSegments = line.Split(":");
+        var setsSegments = inputSegments[1].Split(";");
+
+        gameNum = Convert.ToInt16(Regex.Match(inputSegments[0], @"\d+").Value);
+
+        foreach (var setSegment in setsSegments)
+        {
+            var thrownDiceSegments = setSegment.Split(",");
+
+            foreach (var segment in thrownDiceSegments)
+            {
+                var diceCount = Convert.ToInt16(Regex.Match(segment, @"\d+").Value);
+
+                if (segment.Contains("red") && (diceCount > 12))
+                {
+                    invalidLine = true;
+                }
+                else if (segment.Contains("green") && (diceCount > 13))
+                {
+                    invalidLine = true;
+                }
+                else if (segment.Contains("blue") && (diceCount > 14))
+                {
+                    invalidLine = true;
+                }
+            }
+        }
+
+        if (!invalidLine)
+        {
+            gameSum += gameNum;
+        }
+    }
+}
+
+Console.WriteLine(gameSum);
---
+++ Day2_Cubes_Part2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+var cubePowerSum = 0;
+
+using (var sr = new StreamReader("./Input.txt"))
+{
+    string line;
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        var cubePower = 0;
+        int minRed, minGreen, minBlue;
+        minRed = minGreen = minBlue = 0;
+
+        var inputSegments = line.Split(":");
+        var setsSegments = inputSegments[1].Split(";");
+
+        foreach (var setSegment in setsSegments)
+        {
+            var thrownDiceSegments = setSegment.Split(",");
+
+            foreach (var segment in thrownDiceSegments)
+            {
+                var diceCount = Convert.ToInt16(Regex.Match(segment, @"\d+").Value);
+
+                if (segment.Contains("red") && (diceCount > minRed))
+                {
+                    minRed = diceCount;
+                }
+                else if (segment.Contains("green") && (diceCount > minGreen))
+                {
+                    minGreen = diceCount;
+                }
+                else if (segment.Contains("blue") && (diceCount > minBlue))
+                {
+                    minBlue = diceCount;
+                }
+            }
+        }
+
+        cubePower = minRed * minGreen * minBlue;
+        cubePowerSum += cubePower;
+    }
+}
+
+Console.WriteLine(cubePowerSum);
+Console.ReadKey();
